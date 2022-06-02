c6c6dc6d3ea4fa6df79c765193576015448f12fe
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2022-06-01 22:20:17 -0400

    tfbar

--- src/algo_pg/data/manager.py
+++ src/algo_pg/data/manager.py
@@ -1,6 +1,6 @@
 """
 The data manager will handle interactions between the trading machine/related objects and any
-asset data - either from downloaded CSVs in the top-level data/ folder or from Alpaca itself.
+asset data - either from downloaded CSVs in the top-level data/ folder or from Alpaca itself
 """
 
 

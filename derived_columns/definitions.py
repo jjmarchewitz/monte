9f5a13efc2b306f0c3be2eeae21363d0cb6b4102
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2022-10-09 18:28:32 -0400

    Removed print statements

--- derived_columns/definitions.py
+++ derived_columns/definitions.py
@@ -6,7 +6,6 @@ from derived_columns.decorator import DFIdentifier, derived_column
 @derived_column
 def net_vwap_last_n(ident: DFIdentifier, df: pd.DataFrame, n: int) -> float:
     """DOC:"""
-    print("AAAAAA")
     return df.iloc[-n - 1].vwap - df.iloc[-1].vwap
 
 
@@ -14,8 +13,6 @@ def net_vwap_last_n(ident: DFIdentifier, df: pd.DataFrame, n: int) -> float:
 def avg_vwap_last_n(ident: DFIdentifier, df: pd.DataFrame, n: int) -> float:
     """DOC:"""
     total = 0
-
-    print("BBBBBB")
 
     for i in range(1, n + 1):
         total += df.iloc[-i].vwap
